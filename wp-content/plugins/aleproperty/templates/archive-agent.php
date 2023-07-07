f97da9cef9e66436929c5bd9754cde2df558331b
Aleksander <voronyuk.aleksander@gmail.com>
2023-07-07 14:37:53 +0300

    Output content post type,taxonomy, meta

--- wp-content/plugins/aleproperty/templates/archive-agent.php
+++ wp-content/plugins/aleproperty/templates/archive-agent.php
@@ -1,6 +1,32 @@
 <?php 
 get_header();
+?>
 
-echo 'Archive for Agents';
+<div class="wrapper archive_property">
 
+  <?php
+  if (have_posts()) {
+    while(have_posts()){
+      the_post(); ?>
+
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <?php if(get_the_post_thumbnail(get_the_ID(),'large')){
+        echo get_the_post_thumbnail(get_the_ID(),'medium');
+      } ?>
+        <h2><?php the_title(); ?></h2>
+        <div class="description"><?php the_excerpt(); ?></div>
+        <a href="<?php the_permalink();?>"> <?php esc_html_e('Open this Agent'); ?> </a>
+      </article>
+
+  <?php
+    }
+    //Pagination
+    posts_nav_link();
+  }else{
+    echo '<p>'. esc_html__("No Agents",'aleproperty').'<p>';
+  }
+  ?>
+</div>
+
+<?php
 get_footer();
--- wp-content/plugins/aleproperty/templates/single-property.php
+++ wp-content/plugins/aleproperty/templates/single-property.php
@@ -1,6 +1,51 @@
 <?php 
 get_header();
+?>
 
-echo 'Page for Properties';
+<div class="wrapper single_property">
 
+  <?php
+  if (have_posts()) {
+    while(have_posts()){
+      the_post(); ?>
+
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+      <?php if(get_the_post_thumbnail(get_the_ID(),'large')){
+        echo get_the_post_thumbnail(get_the_ID(),'large');
+      } ?>
+        <h2><?php the_title(); ?></h2>
+        <div class="description"><?php the_content(); ?></div>
+        <div class="property_info">
+          <p class="location"><?php esc_html_e('Location:','aleproperty');
+          
+              $locations = get_the_terms(get_the_ID(),'location');
+              foreach($locations as $location) {
+                echo ' '.$location->name;
+              }
+          ?></p>
+          <p class="type"><?php esc_html_e('Type:','aleproperty');
+          
+          $types = get_the_terms(get_the_ID(),'property-type');
+          foreach($types as $type) {
+            echo ' '.$type->name;
+          }
+          ?></p>
+          <p class="price"><?php esc_html_e('Price:','aleproperty'); echo ' '.get_post_meta(get_the_ID(),'aleproperty_price',true); ?></p>
+          <p class="offer"><?php esc_html_e('Offer:','aleproperty'); echo ' '.get_post_meta(get_the_ID(),'aleproperty_type',true); ?></p>
+          <p class="agent"><?php esc_html_e('Agent:','aleproperty'); 
+          
+              $agent_id = get_post_meta(get_the_ID(),'aleproperty_agent',true);
+              $agent = get_post($agent_id);
+              echo ' '.esc_html__($agent->post_title);
+          ?></p>
+        </div>
+      </article>
+
+  <?php
+    }
+  }
+  ?>
+</div>
+
+<?php
 get_footer();
